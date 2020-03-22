b76354ff93d6f1e8666d9a3a6c291066de90cd63
oumi <lamiaabcr@gmail.com>
2020-03-22 15:16:30 +0100

    put functions correctly

---
+++ functions/index.js
@@ -0,0 +1,46 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
+
+exports.sendNotification = functions.database
+    .ref('/messages/{messageId}/{msgId}')
+    .onWrite(event => {
+    //catch the message data
+        const snapshot = event.after;
+        console.log('inside');
+        const receiver = snapshot.val().to;
+        const sender = snapshot.val().from;
+        const textMessage = snapshot.val().text;
+         console.log('to ', receiver);
+         console.log('from ', sender);
+         console.log('text  ', textMessage);
+        return admin
+              .database()
+              .ref(`users/${sender}`)
+              .once('value')
+              .then(senderData => {
+                    const payload = {
+                      notification: {
+                        title: `${senderData.val().name}`,
+                        body: textMessage
+                          ? textMessage.length <= 100 ? textMessage : textMessage.substring(0, 97) + "..."
+                          : ""
+                         }
+                    };
+                  return admin
+                       .database()
+                       .ref(`users/${receiver}`)
+                       .once('value')
+                       .then(receiverData => {
+                               if (receiverData.val().token) {
+                                           console.log('user token', receiverData.val().token);
+                                           return admin.messaging().sendToDevice(receiverData.val().token, payload);
+                                }else {
+									return null;
+								}
+                             });
+         });
+
+
+    });
